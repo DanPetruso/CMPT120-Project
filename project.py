5a105246913df31151660e0a3cef55288292bc8e
Dan Petruso <daniel.petruso1@marist.edu>
2017-09-27 23:15:45 -0400

    added new functions game, getHelp, and pointChecker. Changed format and a bit of the story to more easily follow rubric. Organized program more and started to make the actual gameplay. Resolves #2, Resolves #3, Resolves #4, Resolves #5, Resolves #6, Resolves #7.

--- project.py
+++ project.py
@@ -1,36 +1,66 @@
 #Dan Petruso
 
+pointGain = 10
+points = 0
+loc0, loc1, loc2, loc3, loc4, loc5 = False
+
 def intro():
-    print("This is a game where you will choose your first Pokemon.")
+    print("This is a game where you will choose your first Pokemon and engage in your first battle.")
     print("Every new move you will gain " + str(pointGain) + " points.")
 
-    print("Professor Oak: 'Welcome to the world of Pokemon, what is your name?")
+    playerName = input("Professor Oak: 'Welcome to the world of Pokemon, what is your name?")
+
+    rivalName = input("Professor Oak: 'Hello " + name + ", this is my grandson and your rival, what was his name again?")
     
-    input("Press enter to respond.")
-    points += pointGain
-    print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
+    print("Professor Oak: 'Oh yes, it was " + rivalsName + ". Great! Now you have begun your journey!/n")
 
-    print("Professor Oak: 'Hello " + name + ", this is my grandson and your rival, what was his name again?'")
-    input("Press enter to respond.")
-    points += pointGain
-    print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
+    print("-------------------------------------------------------------------/n")
 
-    print("Professor Oak: 'Oh yes, it was " + rivalsName + ". Great! Now you have begun your journey!'")
-
-    print("-------------------------------------------------------------------")
+    return playerName, rivalName
 
 def getCopyright():
     print("Copywrite (c) 2107 Daniel Petruso, daniel.petruso1@marist.edu")
-    
-def proj():
-    
-    name = "Red"
-    rivalsName = "Blue"
 
-    loc0, loc1, loc2, loc3, loc4, loc5 = False
+def getHelp():
+    ("Valid commands for the game are North, South, East, West, Help, and Quit.")
+
+def pointChecker():
     
-    points = 0
-    pointGain = 10
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def game():
+    location = [ "You are now in your bedroom. /nTo your north is the stairs.",#0
+                 "You are now in your living room. /nTo the south is the front door. /nTo the north is the stairs to your bedrrom.",#1
+                 "You are now outside. /nTo your east is the tall grass. /nTo your north is your house.",#2
+                 "You are now in the tall grass but Professor Oak stops you and says you cannot continue walking that way without a Pokemon. He invites you to his lab."
+                 "/nTo the south is Oaks lab. /nTo the west is your house.",#3
+                 "You are now in Oaks lab. He tells you to take the pokeball on the desk. /nTo the east is the desk. /nTo the north is the exit.",#4
+                 "You are at the desk and just picked up your pokeball.", rivalName, "wants to battle. /nTo the south is the battle arena. /nTo the north is the exit."]#5
+    
+    gameFinished = False
+    locCount = 0
+
+    while gameFinished == False:
+        
+        #Location0: Bedroom
+        if locCount == 0:
+            while locCount == 0:
+                choice = input(location[0])
+                if choice.lower() == "north":
+                    print("You proceeded down the stairs./n/n")
+                    locCount = 1
+                    
+                elif
+        
+    
+ 
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def main():    
+    
+
     
     playersPokemon = "Charmander"
     rivalsPokemon = "Squirtle"
@@ -38,9 +68,7 @@ def proj():
     attack1 = "Scratch"
     attack2 = "Tackle"
 
-    location = [  ]
-
-    intro()
+    playerName, rivalName = intro()
     
     input("You are now in your room, press enter to continue.")
     points += pointGain
@@ -51,7 +79,9 @@ def proj():
     print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
 
     print("You see Professor Oak run towards you as you approach the grass.")
-    print("Professor Oak: 'Wait, " + name + " don't go there! Wild Pokemon live there. You must have a Pokemon to protect yourself. Come to my lab and I will give you one alongside my grandson" + rivalsName + ".")
+    print("Professor Oak: 'Wait, " + name + " don't go there! Wild Pokemon live there. You must have a Pokemon to protect yourself."
+          "Come to my lab and I will give you one alongside my grandson" + rivalsName + ".")
+                 
     input("Press enter to walk into Oak's lab.")
     points += pointGain
     print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
@@ -98,4 +128,4 @@ def proj():
     
     getCopyright()
     
-proj()
+main()

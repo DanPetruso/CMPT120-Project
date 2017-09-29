5e7ef950201e761d8a85a54f3c429c9a9419695d
Dan Petruso <daniel.petruso1@marist.edu>
2017-09-29 15:01:11 -0400

    Fixed bugs and revoled logical errors.

--- project.py
+++ project.py
@@ -1,81 +1,92 @@
 #Dan Petruso
-
-pointGain = 5
 points = 0
-loc0, loc1, loc2, loc3, loc4, loc5 = False
+loc0, loc1, loc2, loc3, loc4, loc5 = False, False, False, False, False, False
+pointGain = 5
 
 def intro():
     print("This is a game where you will choose your first Pokemon and start your first battle.")
-    print("Every new move you will gain", str(pointGain), "points.")
+    print("Every new move you will gain", pointGain, "points.")
 
-    playerName = input("Professor Oak: 'Welcome to the world of Pokemon, what is your name?'")
+    playerName = input("Professor Oak: 'Welcome to the world of Pokemon, what is your name?' ")
 
-    rivalName = input("Professor Oak: 'Hello " + playerName + ", this is my grandson and your rival, what was his name again?'")
+    rivalName = input("Professor Oak: 'Hello " + playerName + ", this is my grandson and your rival, what was his name again?' ")
     
-    print("Professor Oak: 'Oh yes, it was " + rivalsName + ". Great! Now you have begun your journey!'/n")
+    print("Professor Oak: 'Oh yes, it was " + rivalName + ". Great! Now you have begun your journey!'\n ")
 
-    print("-------------------------------------------------------------------/n")
+    print("-------------------------------------------------------------------\n")
 
     return playerName, rivalName
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def getCopyright():
-    print("Copywrite (c) 2107 Daniel Petruso, daniel.petruso1@marist.edu")
+    print("Copyright (c) 2107 Daniel Petruso, daniel.petruso1@marist.edu")
 
 def getHelp():
-    print("Valid commands for the game are North, South, East, West. /nNot all commands will work for all locations. /nType Help to review the commands and Quit to exit game.")
+    print("\nValid commands for the game are North, South, East, West. \nNot all commands will work for all locations."
+          "\nType Help to review the commands and Quit to exit game.\n")
 
 def getErrorMessage():
-    print("Error. Invalid command.")
+    print("\nError. Invalid command.\n")
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def pointChecker(count):
+    global points
+    global loc0, loc1, loc2, loc3, loc4, loc5
+    global pointGain
+
+    if count == -1: #gives total amount of points to user
+        return points
+    
     if count == 0 and loc0 == False:
         points += pointGain
         loc0 = True
-        print("Points gained:", pointGain, "/nPoint total:", points)
+        print("Points gained:", pointGain, "\nPoint total:", points)
         
     if count == 1 and loc1 == False:
         points += pointGain
         loc1 = True
-        print("Points gained:", pointGain, "/nPoint total:", points)
+        print("Points gained:", pointGain, "\nPoint total:", points)
         
     if count == 2 and loc2 == False:
         points += pointGain
         loc2 = True
-        print("Points gained:", pointGain, "/nPoint total:", points)
+        print("Points gained:", pointGain, "\nPoint total:", points)
         
     if count == 3 and loc3 == False:
         points += pointGain
         loc3 = True
-        print("Points gained:", pointGain, "/nPoint total:", points)
+        print("Points gained:", pointGain, "\nPoint total:", points)
         
     if count == 4 and loc4 == False:
         points += pointGain
         loc4 = True
-        print("Points gained:", pointGain, "/nPoint total:", points)
+        print("Points gained:", pointGain, "\nPoint total:", points)
         
     if count == 5 and loc5 == False:
         points += pointGain
         loc5 = True
-        print("Points gained:", pointGain, "/nPoint total:", points)
+        print("Points gained:", pointGain, "\nPoint total:", points)
+
+
+def getPoints():
+    return pointChecker(-1)
     
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def game():
-    location = [ "You are now in your bedroom. /nTo your east is the door to your living room.",#0
-                 "You are now in your living room. /nTo the south is the front door. /nTo the west is the door to your bedrrom.",#1
-                 "You are now outside. /nTo your east is the tall grass. /nTo your north is your house.",#2
+def game(playerName, rivalName):
+    location = [ "You are now in your bedroom. \nTo your east is the door to your living room.",#0
+                 "You are now in your living room. \nTo the south is the front door. \nTo the west is the door to your bedroom.",#1
+                 "You are now outside. \nTo your east is the tall grass. \nTo your north is your house.",#2
                  "You are now in the tall grass. It is too dangerous to stay here so you will need a Pokemon from Professor Oak to protect yourself."
-                 "/nTo the south is Oak's lab. /nTo the west is your house.",#3
-                 "You are now in Oak's lab. He tells you to take the pokeball from the research room to the west./n"
-                 "To the west is the research room. /nTo the north is the exit.",#4
-                 "You are now in the research room and just picked up your pokeball.", rivalName, "wants to battle./n"
-                 "To the south is the battle arena. /nTo the east is the lab.",#5
-                 "You entered the battle arena and started your first battle. Congratulations", playerName, "you won the game with", str(points), "points!"] #6
+                 "\nTo the south is Oak's lab. \nTo the west is your house.",#3
+                 "You are now in Oak's lab. He tells you to take the pokeball from the research room to the west.\n"
+                 "To the west is the research room. \nTo the north is the exit.",#4
+                 "You are now in the research room and just picked up your pokeball. " + rivalName +  " wants to battle.\n"
+                 "To the south is the battle arena. \nTo the east is the lab.",#5
+                 "You entered the battle arena and started your first battle."] #6
     
     gameFinished = False
     locCount = 0
@@ -89,7 +100,7 @@ def game():
                 choice = input(location[0])
                 
                 if choice.lower() == "east":
-                    print("You proceeded into the living room where your mother is watching television./n/n")
+                    print("You proceeded into the living room where your mother is watching television.\n\n")
                     pointChecker(locCount)
                     locCount+=1
                     
@@ -97,7 +108,8 @@ def game():
                     getHelp()
 
                 elif choice.lower() == "quit":
-                    gameFinished == True
+                    gameFinished = True
+                    print("You just quit the game.")
                     break
 
                 else:
@@ -110,11 +122,11 @@ def game():
                 choice = input(location[1])
                 
                 if choice.lower() == "west":
-                    print("You proceeded back into your room./n/n")
+                    print("You proceeded back into your room.\n\n")
                     locCount-=1
 
                 elif choice.lower() == "south":
-                    print("You headed out the door. /nMom:'Goodbye", playerName + ", have a nice day!/n/n")
+                    print("You headed out the door. \nMom:'Goodbye", playerName + ", have a nice day!\n\n")
                     pointChecker(locCount)
                     locCount+=1
                     
@@ -122,7 +134,8 @@ def game():
                     getHelp()
 
                 elif choice.lower() == "quit":
-                    gameFinished == True
+                    gameFinished = True
+                    print("You just quit the game.")
                     break
 
                 else:
@@ -136,11 +149,11 @@ def game():
                 choice = input(location[2])
                 
                 if choice.lower() == "north":
-                    print("You proceeded back into your house./n/n")
+                    print("You proceeded back into your house.\n\n")
                     locCount-=1
 
                 elif choice.lower() == "east":
-                    print("You headed towards the tall grass. /n/n")
+                    print("You headed towards the tall grass. \n\n")
                     pointChecker(locCount)
                     locCount+=1
                     
@@ -148,7 +161,8 @@ def game():
                     getHelp()
 
                 elif choice.lower() == "quit":
-                    gameFinished == True
+                    gameFinished = True
+                    print("You just quit the game.")
                     break
 
                 else:
@@ -162,11 +176,11 @@ def game():
                 choice = input(location[3])
                 
                 if choice.lower() == "west":
-                    print("You headed back home./n/n")
+                    print("You headed back home.\n\n")
                     locCount-=1
 
                 elif choice.lower() == "south":
-                    print("You headed towards Oak's Lab./n/n")
+                    print("You headed towards Oak's Lab.\n\n")
                     pointChecker(locCount)
                     locCount+=1
                     
@@ -174,7 +188,8 @@ def game():
                     getHelp()
 
                 elif choice.lower() == "quit":
-                    gameFinished == True
+                    gameFinished = True
+                    print("You just quit the game.")
                     break
 
                 else:
@@ -188,11 +203,11 @@ def game():
                 choice = input(location[4])
                 
                 if choice.lower() == "north":
-                    print("You exited the building and started heading towards the tall grass./n/n")
+                    print("You exited the building and started heading towards the tall grass.\n\n")
                     locCount-=1
 
                 elif choice.lower() == "west":
-                    print("You walked over to the research room./n/n")
+                    print("You walked over to the research room.\n\n")
                     pointChecker(locCount)
                     locCount+=1
                     
@@ -200,7 +215,8 @@ def game():
                     getHelp()
 
                 elif choice.lower() == "quit":
-                    gameFinished == True
+                    gameFinished = True
+                    print("You just quit the game.")
                     break
 
                 else:
@@ -213,12 +229,12 @@ def game():
             while locCount == 5:
                 choice = input(location[5])
                 
-                if choice.lower() == "":
-                    print("./n/n")
+                if choice.lower() == "east":
+                    print("You headed back for the lab.\n\n")
                     locCount-=1
 
-                elif choice.lower() == "":
-                    print("./n/n")
+                elif choice.lower() == "south":
+                    print("You walked toward the battle arena.\n\n")
                     pointChecker(locCount)
                     locCount+=1
                     
@@ -226,7 +242,8 @@ def game():
                     getHelp()
 
                 elif choice.lower() == "quit":
-                    gameFinished == True
+                    gameFinished = True
+                    print("You just quit the game.")
                     break
 
                 else:
@@ -236,73 +253,17 @@ def game():
         #Locaton6: Battle Arena and end of game
         if locCount == 6:
             print(location[6])
+            print("Congratulations " + playerName + " you won the game with " + str(getPoints()) + " points!")
             gameFinished = True
 
         
- 
-
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def main(): 
 
     playerName, rivalName = intro()
-    game()
-    
-##    input("You are now in your room, press enter to continue.")
-##    points += pointGain
-##    print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
-##
-##    input("You have just left your room and exited your home. Press enter to walk towards the tall grass.")
-##    points += pointGain
-##    print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
-##
-##    print("You see Professor Oak run towards you as you approach the grass.")
-##    print("Professor Oak: 'Wait, " + name + " don't go there! Wild Pokemon live there. You must have a Pokemon to protect yourself."
-##          "Come to my lab and I will give you one alongside my grandson" + rivalsName + ".")
-##                 
-##    input("Press enter to walk into Oak's lab.")
-##    points += pointGain
-##    print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
-##
-##    print("Professor Oak: 'There are three Pokemon here, Bulbasaur, Charmander, and Squirtle. " + name + ",please choose one first.")
-##    print(rivalsName + ": 'But gramps, why does he get to choose first!")
-##    print("Professor Oak: 'Calm down " + rivalsName + " you get to choose next.")
-##
-##    input("Now it's time to choose Blubasaur, Charmander, or Squirtle, press enter to choose.")
-##    points += pointGain
-##    print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
-##
-##    print(name + " chose " + playersPokemon + "!")
-##    print(rivalsName + ": In that case, I'll choose " + rivalsPokemon + ".")
-##
-##    input("Press enter to leave the lab.")
-##    points += pointGain
-##    print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
-##
-##    print(rivalsName + ": Wait up, we just got our Pokemon, let's battle!")
-##    input("Press enter to send out " + playersPokemon + " to start the battle.")
-##    points += pointGain
-##    print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
-##
-##    print("-------------------------------------------------------------------")
-##    print("Your " + playersPokemon + " is now battling " + rivalsName + "'s " + rivalsPokemon + ".")
-##
-##    input("Press enter to attack.")
-##    points += pointGain
-##    print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
-##
-##    print(playersPokemon + " used ", attack1 + "!")
-##    print(rivalsPokemon + " used ", attack2 + "!")
-##
-##    input("Press enter to attack again.")
-##    points += pointGain
-##    print("You gained " + str(pointGain) + ", your total point count is " + str(points) + ".")
-##
-##    print(playersPokemon + " used ", attack1 + "!")
-##    print(rivalsPokemon + " fainted!")
-##
-##    print("Congratulations, you won the battle!")
-##    print("Your total point count is " + str(points) + ".")
+
+    game(playerName, rivalName)
     
     getCopyright()
     

276245e3456e07559e7978c8f0d3cbb78ca79cd7
Dan Petruso <daniel.petruso1@marist.edu>
2017-09-29 11:52:01 -0400

    Project nearing completion and just needs testing and fine tuning. Added a location6 to be te end of the game and finished game function.

--- project.py
+++ project.py
@@ -1,16 +1,16 @@
 #Dan Petruso
 
-pointGain = 10
+pointGain = 5
 points = 0
 loc0, loc1, loc2, loc3, loc4, loc5 = False
 
 def intro():
-    print("This is a game where you will choose your first Pokemon and engage in your first battle.")
-    print("Every new move you will gain " + str(pointGain) + " points.")
+    print("This is a game where you will choose your first Pokemon and start your first battle.")
+    print("Every new move you will gain", str(pointGain), "points.")
 
     playerName = input("Professor Oak: 'Welcome to the world of Pokemon, what is your name?'")
 
-    rivalName = input("Professor Oak: 'Hello " + name + ", this is my grandson and your rival, what was his name again?'")
+    rivalName = input("Professor Oak: 'Hello " + playerName + ", this is my grandson and your rival, what was his name again?'")
     
     print("Professor Oak: 'Oh yes, it was " + rivalsName + ". Great! Now you have begun your journey!'/n")
 
@@ -69,10 +69,13 @@ def game():
     location = [ "You are now in your bedroom. /nTo your east is the door to your living room.",#0
                  "You are now in your living room. /nTo the south is the front door. /nTo the west is the door to your bedrrom.",#1
                  "You are now outside. /nTo your east is the tall grass. /nTo your north is your house.",#2
-                 "You are now in the tall grass but Professor Oak stops you and tells you it is dangerous to be here wihtout a Pokemon. He invites you to his lab."
-                 "/nTo the south is Oaks lab. /nTo the west is your house.",#3
-                 "You are now in Oaks lab. He tells you to take the pokeball on the desk. /nTo the east is the desk. /nTo the north is the exit.",#4
-                 "You are at the desk and just picked up your pokeball.", rivalName, "wants to battle. /nTo the south is the battle arena. /nTo the north is the exit."]#5
+                 "You are now in the tall grass. It is too dangerous to stay here so you will need a Pokemon from Professor Oak to protect yourself."
+                 "/nTo the south is Oak's lab. /nTo the west is your house.",#3
+                 "You are now in Oak's lab. He tells you to take the pokeball from the research room to the west./n"
+                 "To the west is the research room. /nTo the north is the exit.",#4
+                 "You are now in the research room and just picked up your pokeball.", rivalName, "wants to battle./n"
+                 "To the south is the battle arena. /nTo the east is the lab.",#5
+                 "You entered the battle arena and started your first battle. Congratulations", playerName, "you won the game with", str(points), "points!"] #6
     
     gameFinished = False
     locCount = 0
@@ -86,9 +89,9 @@ def game():
                 choice = input(location[0])
                 
                 if choice.lower() == "east":
-                    print("You proceeded into the living room where your mother is cooking breakfast./n/n")
-                    pointChecker()
-                    locCount = 1
+                    print("You proceeded into the living room where your mother is watching television./n/n")
+                    pointChecker(locCount)
+                    locCount+=1
                     
                 elif choice.lower() == "help":
                     getHelp()
@@ -108,12 +111,12 @@ def game():
                 
                 if choice.lower() == "west":
                     print("You proceeded back into your room./n/n")
-                    locCount = 0
+                    locCount-=1
 
                 elif choice.lower() == "south":
                     print("You headed out the door. /nMom:'Goodbye", playerName + ", have a nice day!/n/n")
-                    pointChecker()
-                    locCount = 2
+                    pointChecker(locCount)
+                    locCount+=1
                     
                 elif choice.lower() == "help":
                     getHelp()
@@ -134,12 +137,12 @@ def game():
                 
                 if choice.lower() == "north":
                     print("You proceeded back into your house./n/n")
-                    locCount = 1
+                    locCount-=1
 
                 elif choice.lower() == "east":
-                    print("You headed towards the tall grass")
-                    pointChecker()
-                    locCount = 3
+                    print("You headed towards the tall grass. /n/n")
+                    pointChecker(locCount)
+                    locCount+=1
                     
                 elif choice.lower() == "help":
                     getHelp()
@@ -150,21 +153,100 @@ def game():
 
                 else:
                     getErrorMessage()
+
+
+        #Location3: Tall Grass
+        if locCount == 3:
+
+            while locCount == 3:
+                choice = input(location[3])
+                
+                if choice.lower() == "west":
+                    print("You headed back home./n/n")
+                    locCount-=1
+
+                elif choice.lower() == "south":
+                    print("You headed towards Oak's Lab./n/n")
+                    pointChecker(locCount)
+                    locCount+=1
+                    
+                elif choice.lower() == "help":
+                    getHelp()
+
+                elif choice.lower() == "quit":
+                    gameFinished == True
+                    break
+
+                else:
+                    getErrorMessage()
+
+
+        #Location4: Oak's Lab
+        if locCount == 4:
+
+            while locCount == 4:
+                choice = input(location[4])
+                
+                if choice.lower() == "north":
+                    print("You exited the building and started heading towards the tall grass./n/n")
+                    locCount-=1
+
+                elif choice.lower() == "west":
+                    print("You walked over to the research room./n/n")
+                    pointChecker(locCount)
+                    locCount+=1
+                    
+                elif choice.lower() == "help":
+                    getHelp()
+
+                elif choice.lower() == "quit":
+                    gameFinished == True
+                    break
+
+                else:
+                    getErrorMessage()
+
+
+        #Location5: Reseach Room
+        if locCount == 5:
+
+            while locCount == 5:
+                choice = input(location[5])
+                
+                if choice.lower() == "":
+                    print("./n/n")
+                    locCount-=1
+
+                elif choice.lower() == "":
+                    print("./n/n")
+                    pointChecker(locCount)
+                    locCount+=1
+                    
+                elif choice.lower() == "help":
+                    getHelp()
+
+                elif choice.lower() == "quit":
+                    gameFinished == True
+                    break
+
+                else:
+                    getErrorMessage()
+
+
+        #Locaton6: Battle Arena and end of game
+        if locCount == 6:
+            print(location[6])
+            gameFinished = True
+
+        
  
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def main():    
-    
-
-    
-    playersPokemon = "Charmander"
-    rivalsPokemon = "Squirtle"
-
-    attack1 = "Scratch"
-    attack2 = "Tackle"
+def main(): 
 
     playerName, rivalName = intro()
+    game()
     
 ##    input("You are now in your room, press enter to continue.")
 ##    points += pointGain
